2225f28478b9aa071c7d333a759f047accee0693
Alex Wilson <fonzmp@gmail.com>
2018-05-18 16:42:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
     if @discount == 0
       @total
     else
-      @total = @total.to_f - (@discount.to_f / 100.to_f)
+      new_total = @total.to_f - (@discount.to_f / 100.to_f)
     end
   end
 
