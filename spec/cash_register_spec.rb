1c884b5f3e032d5a3a7d960ba7f56ff22b0cf495
Alex Wilson <fonzmp@gmail.com>
2018-05-18 17:09:52 +0000

    automatically backed up by learn

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,9 +1,6 @@
 describe 'CashRegister' do
   let(:cash_register) { CashRegister.new }
   let(:cash_register_with_discount) { CashRegister.new(20) }
-  before(:each)
-    @@CART = {}
-  end
 
   describe '::new' do
     it 'sets an instance variable @total on initialization to zero' do
